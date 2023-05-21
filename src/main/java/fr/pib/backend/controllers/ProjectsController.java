e31432c0a44a0ad5e66ec99cbbd915cd6d28f8e6
Gourgen KHACHATRIAN <gugo2828@gmail.com>
2023-05-21 18:28:05 +0200

    Project 68,crud only for projects realised apart delete function in controller and service

--- src/main/java/fr/pib/backend/controllers/ProjectsController.java
+++ src/main/java/fr/pib/backend/controllers/ProjectsController.java
@@ -6,9 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import fr.pib.backend.beans.Projects;
 import fr.pib.backend.services.ProjectsService;
 
@@ -24,5 +30,23 @@ public class ProjectsController {
     @ResponseStatus(code = HttpStatus.OK)
     public List<Projects> findAll(){
         return this.projectsService.getAllProjects();
+    }
+    
+    //create projects    
+    @PostMapping("")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Projects create(@RequestBody Projects projects) {
+        return this.projectsService.create(projects);
+    }
+    
+    //update projects    
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Projects update(@RequestBody Projects projects, @PathVariable("id") Long id) {
+        if (!id.equals(projects.getId())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Mauvaise session à mettre à jour");
+        }
+        return this.projectsService.update(projects);
     }
 }
--- src/main/java/fr/pib/backend/services/ProjectsService.java
+++ src/main/java/fr/pib/backend/services/ProjectsService.java
@@ -3,7 +3,10 @@ package fr.pib.backend.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import fr.pib.backend.beans.Projects;
 import fr.pib.backend.repositories.ProjectsRepository;
 
@@ -14,9 +17,23 @@ public class ProjectsService {
     @Autowired
     private ProjectsRepository projectsRepository;
     
-// get all Projects
+    // get all Projects
     
     public List<Projects> getAllProjects(){
         return projectsRepository.findAll();
     }  
+    
+    //create projects
+    public Projects create(Projects projects) {
+        return this.projectsRepository.save(projects);
+    }
+    
+    //update projects
+    public Projects update(Projects projects) {
+        if (!this.projectsRepository.existsById(projects.getId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Impossible de trouver le projet à mettre à jour");
+        }
+        return this.projectsRepository.save(projects);
+    }
 }
