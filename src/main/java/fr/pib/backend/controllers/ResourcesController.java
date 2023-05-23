e0f02a1452f5c243c425e1abc096b55e23bc88ad
Gourgen KHACHATRIAN <gugo2828@gmail.com>
2023-05-23 10:17:26 +0200

    Project 68,backend resource crud ok apart delete

--- src/main/java/fr/pib/backend/controllers/ResourcesController.java
+++ src/main/java/fr/pib/backend/controllers/ResourcesController.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import fr.pib.backend.beans.Resources;
 import fr.pib.backend.services.ResourcesService;
 
@@ -24,5 +29,24 @@ public class ResourcesController {
     @ResponseStatus(code = HttpStatus.OK)
     public List<Resources> findAll(){
         return this.resourcesService.getAllResources();
+    }
+    
+    //create resources    
+    @PostMapping("")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Resources create(@RequestBody Resources resources) {
+        System.out.println(resources);
+        return this.resourcesService.create(resources);
+    }
+    
+    //update resources    
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.ACCEPTED)
+    public Resources update(@RequestBody Resources resources, @PathVariable("id") Long id) {
+        if (!id.equals(resources.getId())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Mauvaise session à mettre à jour");
+        }
+        return this.resourcesService.update(resources);
     }
 }
--- src/main/java/fr/pib/backend/services/ResourcesService.java
+++ src/main/java/fr/pib/backend/services/ResourcesService.java
@@ -3,7 +3,11 @@ package fr.pib.backend.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+
 import fr.pib.backend.beans.Resources;
 import fr.pib.backend.repositories.ResourcesRepository;
 
@@ -19,4 +23,18 @@ public class ResourcesService {
     public List<Resources> getAllResources(){
         return resourcesRepository.findAll();
     }  
+    
+    //create Resources
+    public Resources create(Resources resources) {
+        return this.resourcesRepository.save(resources);
+    }
+    
+    //update Resources
+    public Resources update(Resources resources) {
+        if (!this.resourcesRepository.existsById(resources.getId())) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Impossible de trouver le resource à mettre à jour");
+        }
+        return this.resourcesRepository.save(resources);
+    }
 }
